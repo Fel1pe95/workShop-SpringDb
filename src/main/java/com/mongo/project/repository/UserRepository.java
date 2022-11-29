b1176803847334264d141a278854bad767cdf580
Luis Felipe <lipe.arraial1995@gmail.com>
2022-11-29 08:30:45 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/mongo/project/repository/UserRepository.java
+++ src/main/java/com/mongo/project/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.mongo.project.repository;
 
-public class UserRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mongo.project.entities.User;
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
 
 }
--- src/main/java/com/mongo/project/resources/UserResource.java
+++ src/main/java/com/mongo/project/resources/UserResource.java
@@ -1,10 +1,28 @@
 package com.mongo.project.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mongo.project.entities.User;
+import com.mongo.project.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
 }
