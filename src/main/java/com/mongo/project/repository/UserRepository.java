bc3b9241819135cd8b217db4caa9e088391806e2
Luis Felipe <lipe.arraial1995@gmail.com>
2022-11-30 08:08:59 -0300

    obtendo usuario por id

--- src/main/java/com/mongo/project/repository/UserRepository.java
+++ src/main/java/com/mongo/project/repository/UserRepository.java
@@ -7,4 +7,5 @@ import com.mongo.project.entities.User;
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
 
+
 }
--- src/main/java/com/mongo/project/service/UserService.java
+++ src/main/java/com/mongo/project/service/UserService.java
@@ -1,12 +1,14 @@
 package com.mongo.project.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mongo.project.entities.User;
 import com.mongo.project.repository.UserRepository;
+import com.mongo.project.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,13 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+		
 	}
 
 }
