499fd0286e9b869e84a41f5ebfbd3b03c9acebcc
Luis Felipe <lipe.arraial1995@gmail.com>
2022-12-02 09:56:37 -0300

    Consulta simples com query methods

--- src/main/java/com/mongo/project/resources/PostResource.java
+++ src/main/java/com/mongo/project/resources/PostResource.java
@@ -1,12 +1,16 @@
 package com.mongo.project.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mongo.project.entities.Post;
+import com.mongo.project.resources.util.URL;
 import com.mongo.project.service.PostService;
 
 @RestController
@@ -22,4 +26,11 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 
+	@RequestMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
--- src/main/java/com/mongo/project/service/PostService.java
+++ src/main/java/com/mongo/project/service/PostService.java
@@ -1,5 +1,6 @@
 package com.mongo.project.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,11 @@ public class PostService {
 		Optional<Post> post = postRepository.findById(id);
 		return post.orElseThrow(()-> new ObjectNotFoundException("não encontrado"));
 	
+	}
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
+		
 	}
 	
 }
